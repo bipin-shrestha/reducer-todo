489ad1baa9558b07782e2eccaa66d085341e9696
bipin-shrestha <iambpn.shrestha@gmail.com>
2021-01-29 20:41:59 -0800

    completed

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,28 +1,37 @@
-import logo from './logo.svg';
-import reducer from "react";
 import './App.css';
+import React, { useState, useReducer } from 'react';
+import reducer, { TodoList } from './reducers/simpleReducer';
+import Todo from './components/Todo';
+
+export const ACTIONS ={
+  ADD_TODO: 'add-todo',
+  TOGGLE_TODO: 'toggle-todo',
+  DELETE_TODO: 'delete-todo'
+}
 
 
-// const initialState = {
-
-// }
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const [todos, dispatch] = useReducer(reducer, TodoList );
+const [item, setItem] = useState('');
+   
+function handleSubmit(e) {
+   e.preventDefault();
+   dispatch({type: ACTIONS.ADD_TODO, payload: {item: item} });
+   setItem('');
+    }
+   
+
+return (
+  <div className="App">
+      <form>
+        <input type="text" value={item} onChange={e => setItem(e.target.value)}/>
+        <button onClick={handleSubmit}>Submit</button>
+      </form>
+     <div>
+      {todos.map(todo => {
+      return <Todo key={todo.id} todo={todo} dispatch={dispatch} />
+      })}
+      </div>
     </div>
   );
 }
--- todo/src/reducers/simpleReducer.js
+++ todo/src/reducers/simpleReducer.js
@@ -1,27 +1,44 @@
-export const Todo = [
-  {
-    item: 'Learn about reducers',
-    completed: false,
-    id: 3892987589
-  },
-  {
-    item: 'Learn about switch',
-    completed: false,
-    id: 3892987590
-  },
-  {
-    item: 'Learn about action',
-    completed: true,
-    id: 3892987591
-  }
-]
+import { ACTIONS } from "../App";
 
-
-
-const reducer = (state, action) => {
-    switch(action.type){
-        default: return state;
+export const TodoList = [
+    {
+      item: 'Learn about reducers',
+      completed: false,
+      id: 3892987589
+    },
+    {
+      item: 'Learn about switch',
+      completed: false,
+      id: 3892987590
+    },
+    {
+      item: 'Learn about action',
+      completed: true,
+      id: 3892987591
     }
-};
+  ]
 
+function newTodo(item){
+    return { id: Date.now(), item: item, completed: false }        
+  }
+
+const reducer = (todos, action) => {
+    switch(action.type){
+      case ACTIONS.ADD_TODO: 
+         return [...todos, newTodo(action.payload.item)]
+      case ACTIONS.TOGGLE_TODO: 
+         return todos.map(todo => {
+        if(todo.id === action.payload.id){
+         return {...todo, completed: !todo.completed}
+          }
+         return todo;
+        })
+      case ACTIONS.DELETE_TODO:
+         return todos.filter(todo => todo.id != action.payload.id)
+      default: return ;
+         
+    }  
+     
+  }
+  
 export default reducer;
